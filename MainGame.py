469a19abf7acb42ae1e0a73929927d69a4bf7e6a
Murad Farajov <farajovmuradvaleh@gmail.com>
2024-02-07 18:47:11 +0100

    ...

--- MainGame.py
+++ MainGame.py
@@ -1,13 +1,13 @@
-import os
 from Live import welcome, load_game
 from MemoryGame import *
 from GuessGame import *
 from CurrencyRouletteGame import *
+import os
 
 
 def main():
-    # player_name = input("Please enter your name: ")
-    player_name = os.getenv('PLAYER_NAME', 'Murad Farajov')
+    player_name = input("Please enter your name: ")
+    # player_name = os.getenv('PLAYER_NAME', 'Murad Farajov')
     welcome(player_name)
     difficulty_levels = range(1, 100)
     load_game(GameRegistry.games, difficulty_levels)
