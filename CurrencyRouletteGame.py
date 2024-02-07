97193ab51ece1d7a4814327fb84ddac4229020a6
Murad Farajov <farajovmuradvaleh@gmail.com>
2024-02-07 21:00:07 +0100

    Added Jenkins parameters for CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,6 +1,7 @@
 from Games import Game
 from GameRegistry import GameRegistry
 import random
+from inputs import get_input
 
 
 class CurrencyRouletteGame(Game):
@@ -26,13 +27,14 @@ class CurrencyRouletteGame(Game):
     @staticmethod
     def get_guess_from_user(amount_in_usd):
         print(f"How much do you think {amount_in_usd} USD is in ILS?")
-        return float(input("Enter your guess: "))
+        # return float(input("Enter your guess: "))
+        return get_input('CURRENCY_GUESS', "Enter your guess: ", float)
 
     def play(self):
         amount_in_usd = random.randint(1, 100)
         interval = self.get_money_interval(amount_in_usd)
         user_guess = self.get_guess_from_user(amount_in_usd)
-        if interval[1] <= user_guess <= interval[0]:
+        if interval[0] <= user_guess <= interval[1]:
             print('Congratulations, You won!')
         else:
             print('Unfortunately You lost!')
