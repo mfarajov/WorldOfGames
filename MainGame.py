0b7656d8de726ffd0565e573c87a68af9ba5a0ac
Murad Farajov <farajovmuradvaleh@gmail.com>
2024-02-14 15:38:21 +0100

    Updated requirements.txt

--- MainGame.py
+++ MainGame.py
@@ -5,7 +5,7 @@ from CurrencyRouletteGame import *
 
 
 def main():
-    player_name = input("Please enter your name: ")
+    player_name = get_input("PLAYER_NAME", "Please enter your name: ", str)
     welcome(player_name)
     difficulty_levels = range(1, 100)
     load_game(GameRegistry.games, difficulty_levels)
